0e173a70f8622821f95f65d84db121e50f0b3443
varshnee <varshneeld@gmail.com>
2023-07-17 14:41:31 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import bakery from '../assets/bakery_logo.jpg'; 
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="left">
+        <img src={bakery}/>
+      </div>
+      <div className="right">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
